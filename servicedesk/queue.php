447b503187a1631c96a90b23978452314dd1a1e3
Denis Zenkin <astron4ik.94@gmail.com>
2020-08-24 18:12:20 +0300

    Update queue.php

--- servicedesk/queue.php
+++ servicedesk/queue.php
@@ -1,83 +1,72 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Инструмент для работы с уведомлениями");
-if (CModule::IncludeModule("im"))
+$APPLICATION->SetTitle("Инструмент для работы с очередью задач");
+if (CModule::IncludeModule("tasks"))
 {
-	$rsTask = array();
-	$CIMNElement = new CIMNotify;
-	$rsTask = $CIMNElement->GetNotifyList();
-	$NotifyCounter = $CIMNElement->GetNotifyCounter();
-	$value = reset($rsTask);
-	$NoDublicat = array();
+	global $USER;
+	
+	// Получаем список активных задач на очереди - Пользователь: Тех. Поддержка (232)
+	$res = CTasks::GetList(
+        Array("TITLE" => "ASC"), 
+        Array(
+			"RESPONSIBLE_ID" => "232",
+			"REAL_STATUS" => "2")
+	);
 
-	for ($i = 1; $i <= 15; $i++)
+	// Делегирование задачи на ответственного в отделе
+	if ($_POST['getTask']) 
 	{
-		$arParams['PAGE'] = $i;
-		$result = $CIMNElement->GetNotifyList($arParams);
-		$rsTask = $rsTask + $result;
-	}
-
-	if ($_POST['id']) {
-		for ($row = $value['id']; $row > 0; $row--) {
-			if ($rsTask[$row] != null) {
-				if ($rsTask[$row]['read'] == N) {
-					$idTask2= explode("|", $rsTask[$row]['originalTag']);
-					if ($idTask2[2] == $_POST['id']) {
-						$SetReadNotifity = $CIMNElement->MarkNotifyRead($rsTask[$row]['id']);
-					}
-				}
-			}
+		$arFields = Array("RESPONSIBLE_ID" => $USER->GetID());
+		$obTask = new CTasks;
+		$success = $obTask->Update($_POST['id'], $arFields);
+		if($success)
+		{
+			echo '<pre>';
+			echo "Вы можете начать работу по задаче.<br>";
+			echo "Ссылка на задачу: <a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/", $_POST['id'], "/\">Здесь</a>";
+			die();
 		}
-		header("Location:https://in.autokontinent.ru/forms/notify.php");
+		else
+		{
+			if($e = $APPLICATION->GetException())
+			echo "Error: ".$e->GetString();
+		}
 	}
 	else {
-		echo "Выберите строку для удаления уведомлений по задаче";
+		echo "Вы можете получить любую задачу из очереди";
 	}
 
+	// Таблица для отображения списка задач
 	echo '<table class="intask-main data-table">';
-	echo '<legend>Список уведомлений. Количество уведомлений: '.$NotifyCounter.' </legend>';
+	echo '<legend>Список задач в очереди: </legend>';
 	echo '<thead>';
 	echo '<tr class=\"intask-row selected\">
-			<td class=\"intask-cell\">№</td>
-			<td class=\"intask-cell\">UserName</td>
-			<td class=\"intask-cell\">text</td>
-			<td class=\"intask-cell\">time</td>
-			<td class=\"intask-cell\">date</td>
-			<td class=\"intask-cell\">idTask</td>
+			<td class=\"intask-cell\">№ Задачи</td>
+			<td class=\"intask-cell\">Постановщик</td>
+			<td class=\"intask-cell\">Задача</td>
+			<td class=\"intask-cell\">Крайний срок</td>
 			<td>Действие</td>
 		</tr>';
 	echo '</thead>';
-	for ($row = $value['id']; $row > 0; $row--) 
+	
+	while ($arTask = $res->GetNext()) 
 	{
-		if ($rsTask[$row] != null)
+		if ($arTask != null)
 		{
-			$idTask= explode("|", $rsTask[$row]['originalTag']);
-			$rsTask[$row]['TaskID'] = $idTask[2];
-			if ($rsTask[$row]['read'] == N)
-			{
-				if (in_array($rsTask[$row]['TaskID'], $NoDublicat) != true)
-				{
-					$NoDublicat[] = $rsTask[$row]['TaskID'];
-					echo '<form action=/forms/notify.php method="post">';
-					echo "<tr class=\"intask-row\">";
-					echo "<td class=\"intask-cell\">".$rsTask[$row]['id']."</td>";
-					echo "<td class=\"intask-cell\">".$rsTask[$row]['userName']."</td>";
-					echo "<td class=\"intask-cell\">".$rsTask[$row]['text']."</td>";
-					echo "<td style=\"min-width: 100px;\" class=\"intask-cell\">".$rsTask[$row]['date']->toString(new \Bitrix\Main\Context\Culture(array("FORMAT_DATETIME" => "HH:MI")))."</td>";
-					echo "<td style=\"min-width: 100px;\"  class=\"intask-cell\">".$rsTask[$row]['date']->toString(new \Bitrix\Main\Context\Culture(array("FORMAT_DATETIME" => "DD-MM-YYYY")))."</td>";
-					echo "<td class=\"intask-cell\">".$rsTask[$row]['TaskID']."</td>";
-					echo "<input type=\"hidden\" name=\"id\" value=\"" . $rsTask[$row]['TaskID'] . "\">";
-					echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\"
-type=\"submit\" style=\"cursor:pointer;\" value=\"Прочитать все уведомления по задаче\"/>Прочитать все уведомления по задаче</button></td>";
-					echo "</tr>";
-					echo "</form>";
-				}
-			}
+			echo '<form action=/servicedesk/queue.php method="post">';
+			echo "<tr class=\"intask-row\">";
+			echo "<td class=\"intask-cell\">".$arTask['ID']."</td>";
+			echo "<td class=\"intask-cell\">".$arTask['CREATED_BY_LAST_NAME'], " ", $arTask['CREATED_BY_NAME'], " ",$arTask['CREATED_BY_SECOND_NAME']."</td>";
+			echo "<td class=\"intask-cell\">".$arTask['TITLE']."</td>";
+			echo "<td style=\"min-width: 100px;\" class=\"intask-cell\">".$arTask['DEADLINE']."</td>";
+			echo "<input type=\"hidden\" name=\"id\" value=\"".$arTask['ID']."\">";
+			echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\" type=\"submit\" style=\"cursor:pointer;\" name=\"getTask\" value=\"getTask\"/>Начать работу по задаче</button></td>";
+			echo "</tr>";
+			echo "</form>";
 		}
 	}
 	echo '</table>';
 }
-
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
