9099ce6bdc9b0f256fe80e61e4a1def5eeabeb90
Denis Zenkin <astron4ik.94@gmail.com>
2020-08-24 18:12:45 +0300

    Update request.php

--- servicedesk/request.php
+++ servicedesk/request.php
@@ -8,21 +8,21 @@ $APPLICATION->SetTitle("Заявка на Тех. Поддержку");
 <legend>Заполните форму</legend>
 <table border="0" width="500px">
 	<tr><td>Программа: </td><td>
-	<select name="tel" width="100%" required>
+	<select name="app" width="100%" required>
 		<option value="">Выберите программу</option>
 		<option value="site">Сайт</option>
 		<option value="wms">WMS Solvo</option>
 		<option value="ax">MSDAX Axapta</option>
 	</select></td></tr>
 	<tr><td>Тип обращения: </td><td>
-	<select name="tel" width="100%" required>
-		<option value="incident" selected>Инцидент</option>
-		<option value="consultation">Консультация</option>
+	<select name="type_req" width="100%" required>
+		<option value="Инцидент" selected>Инцидент</option>
+		<option value="Консультация">Консультация</option>
 	</select></td></tr>
 	<tr><td>Тема обращения:</td><td> <input type="text" name="small_description" required /></td></tr>
 	<tr><td><label for="description">Описание проблемы</label></td>
 	<td><textarea name="description" required></textarea></td></tr>
-	<tr><td>Срочно?</td><td> <input type="checkbox" name="quickly" value="quickly">
+	<tr><td>Срочно?</td><td> <input type="checkbox" name="quickly" value="true">
 	<tr><td></td><td>*После создания задачи, все исправления указываем в комментария к задаче.</td></tr>
 	<tr><td></td><td>*Описание задачи НЕ РЕДАКТИРУЕМ !!!</td></tr>
 </table>
