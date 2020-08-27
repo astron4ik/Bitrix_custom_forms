e5a0a00ea5049fa8348f03a2c2dc7f541fc4fd6c
Denis Zenkin <astron4ik.94@gmail.com>
2020-08-27 12:46:35 +0300

    restore file

--- servicedesk/queue.php
+++ servicedesk/queue.php
@@ -1,111 +1,72 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-?>
-<?php
-
-global $APPLICATION;
-
-//Функция отладки массива
-function debug()
-{
-	echo "<pre>";
-	echo print_r($_POST);
-	echo "</pre>";
-}
-
-// Функция "Заявка в ТП"
-function create_request()
+$APPLICATION->SetTitle("Инструмент для работы с очередью задач");
+if (CModule::IncludeModule("tasks"))
 {
 	global $USER;
 	
-	// Назначение ответственного для задачи
-	if ($_POST['app'] == 'site')
-	{
-		$RESPONSIBLE_ID = 118; // По сайту, на Максима Лютова
-		$addTitle = '[Сайт]';
-		
-		// Список наблюдателей
-		$arAuditors = array(35, $USER->GetID()); // Авдеев
-		
-		// Теги к задаче
-		$Tags = 'WEBServiceDesk';
-	}
-	elseif ($_POST['app'] == 'wms')
-	{
-		$RESPONSIBLE_ID = 232; // WMS - на очередь
-		$addTitle = '[WMS Solvo]';
-		
-		//Список наблюдателей
-		$arAuditors = array(98, 31, 277, $USER->GetID()); // Макаров, Кириченко, Ратникова
-		
-		// Теги к задаче
-		$Tags = 'WMSServiceDesk';
-	}
-	elseif ($_POST['app'] == 'ax')
-	{
-		$RESPONSIBLE_ID = 232; // AX - на очередь
-		$addTitle = '[MSDAX Axapta]';
-		
-		//Список наблюдателей
-		$arAuditors = array(98, 31, 277, $USER->GetID()); // Макаров, Кириченко, Ратникова
-		
-		// Теги к задаче
-		$Tags = 'DAXServiceDesk';
-	}
-	
-	//Формироване дейдлайна
-	$deadline = date('d.m.Y H:i:s', strtotime($Realtime.' +2 hours'));
-	
-	//Формирование заголовка задачи
-	ob_start();
-	echo "[ТехПоддержка]-", $addTitle, "-", $_POST['small_description'];
-	$TitleVar = ob_get_contents();
-	ob_end_clean();
-	
-	//Формирование описание задачи
-	ob_start();
-	echo "<b>Программа</b>: ", $addTitle, "<br>",
-		"<b>Описание проблемы</b>: ", $_POST['description'], "<br>",
-		"<b>Тег</b>: ", $Tags, "<br>";
-	$TaskDescription = ob_get_contents();
-	ob_end_clean();
-	
-	//массив структуры задачи
-	$arFields = Array(
-		"TITLE" => $TitleVar,
-		"SITE_ID" => "s1",
-		"DESCRIPTION" => $TaskDescription,
-		"RESPONSIBLE_ID" => $RESPONSIBLE_ID,
-		"AUDITORS" => $arAuditors,
-		"CREATED_BY" => $USER->GetID(),
-		"DEADLINE" => $deadline,
-		"TAGS" => $Tags,
-		"TASK_CONTROL" => "Y",
-		"ALLOW_CHANGE_DEADLINE" => "Y"
-    );
-	
-	//Добавление задачи и получение ID
-	$obTask = new CTasks;
-	$ID = $obTask->Add($arFields);
+	// Получаем список активных задач на очереди - Пользователь: Тех. Поддержка (232)
+	$res = CTasks::GetList(
+        Array("TITLE" => "ASC"), 
+        Array(
+			"RESPONSIBLE_ID" => "232",
+			"REAL_STATUS" => "2")
+	);
 
-	echo '<pre>';
-	echo "Задача в Тех. Поддержку создана: ", $ID, "<br>";
-	echo "Ссылка на задачу: <a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/", $ID, "/\">Здесь</a>";
-	echo "А так же, вы можете посмотреть задачи в очереди: <a href=\"https://in.autokontinent.ru/servicedesk/queue.php\">Здесь</a>";
+	// Делегирование задачи на ответственного в отделе
+	if ($_POST['getTask']) 
+	{
+		$arFields = Array("RESPONSIBLE_ID" => $USER->GetID());
+		$obTask = new CTasks;
+		$success = $obTask->Update($_POST['id'], $arFields);
+		if($success)
+		{
+			echo '<pre>';
+			echo "Вы можете начать работу по задаче.<br>";
+			echo "Ссылка на задачу: <a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/", $_POST['id'], "/\">Здесь</a>";
+			die();
+		}
+		else
+		{
+			if($e = $APPLICATION->GetException())
+			echo "Error: ".$e->GetString();
+		}
+	}
+	else {
+		echo "Вы можете получить любую задачу из очереди";
+	}
+
+	// Таблица для отображения списка задач
+	echo '<table class="intask-main data-table">';
+	echo '<legend>Список задач в очереди: </legend>';
+	echo '<thead>';
+	echo '<tr class=\"intask-row selected\">
+			<td class=\"intask-cell\">№ Задачи</td>
+			<td class=\"intask-cell\">Постановщик</td>
+			<td class=\"intask-cell\">Задача</td>
+			<td class=\"intask-cell\">Крайний срок</td>
+			<td>Действие</td>
+		</tr>';
+	echo '</thead>';
+	
+	while ($arTask = $res->GetNext()) 
+	{
+		if ($arTask != null)
+		{
+			echo '<form action=/servicedesk/queue.php method="post">';
+			echo "<tr class=\"intask-row\">";
+			echo "<td class=\"intask-cell\">".$arTask['ID']."</td>";
+			echo "<td class=\"intask-cell\">".$arTask['CREATED_BY_LAST_NAME'], " ", $arTask['CREATED_BY_NAME'], " ",$arTask['CREATED_BY_SECOND_NAME']."</td>";
+			echo "<td class=\"intask-cell\">".$arTask['TITLE']."</td>";
+			echo "<td style=\"min-width: 100px;\" class=\"intask-cell\">".$arTask['DEADLINE']."</td>";
+			echo "<input type=\"hidden\" name=\"id\" value=\"".$arTask['ID']."\">";
+			echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\" type=\"submit\" style=\"cursor:pointer;\" name=\"getTask\" value=\"getTask\"/>Начать работу по задаче</button></td>";
+			echo "</tr>";
+			echo "</form>";
+		}
+	}
+	echo '</table>';
 }
 
-//Обработка вызова функции дебага
-if (isset($_POST['debug_button']))
-{
-	$APPLICATION->SetTitle("Debug Page");
-	debug();
-}
-
-// Обработка вызова функции "Заявка в ТП"
-if (isset($_POST['request']))
-{
-	$APPLICATION->SetTitle("Заявка создана");
-	create_request();
-}
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
