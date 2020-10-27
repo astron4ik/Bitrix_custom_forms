2618b7a0dfc2c04ed2ae8c2b56e4fb0ac8383e6c
Denis Zenkin <astron4ik.94@gmail.com>
2020-10-27 22:24:51 +0300

    Update queue.php

--- servicedesk/queue.php
+++ servicedesk/queue.php
@@ -7,11 +7,14 @@ if (CModule::IncludeModule("tasks"))
 	
 	// Получаем список активных задач на очереди - Пользователь: Тех. Поддержка (232)
 	$res = CTasks::GetList(
-        Array("TITLE" => "ASC"), 
+        Array(
+			"PRIORITY" => "DESC",
+			"DEADLINE" => "ASC"
+		), 
         Array(
 			"RESPONSIBLE_ID" => "232",
-			"REAL_STATUS" => "2")
-	);
+			"REAL_STATUS" => array(CTasks::STATE_NEW, CTasks::STATE_PENDING, CTasks::STATE_IN_PROGRESS))
+		);
 
 	// Делегирование задачи на ответственного в отделе
 	if ($_POST['getTask']) 
@@ -42,27 +45,46 @@ if (CModule::IncludeModule("tasks"))
 	echo '<thead>';
 	echo '<tr class=\"intask-row selected\">
 			<td class=\"intask-cell\">№ Задачи</td>
+			<td class=\"intask-cell\">Время создания</td>
 			<td class=\"intask-cell\">Постановщик</td>
 			<td class=\"intask-cell\">Задача</td>
 			<td class=\"intask-cell\">Крайний срок</td>
 			<td>Действие</td>
 		</tr>';
 	echo '</thead>';
-	
+
 	while ($arTask = $res->GetNext()) 
 	{
 		if ($arTask != null)
 		{
-			echo '<form action=/servicedesk/queue.php method="post">';
-			echo "<tr class=\"intask-row\">";
-			echo "<td class=\"intask-cell\">".$arTask['ID']."</td>";
-			echo "<td class=\"intask-cell\">".$arTask['CREATED_BY_LAST_NAME'], " ", $arTask['CREATED_BY_NAME'], " ",$arTask['CREATED_BY_SECOND_NAME']."</td>";
-			echo "<td class=\"intask-cell\"><a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/",$arTask['ID'],"/\">",$arTask['TITLE'],"</a></td>";
-			echo "<td style=\"min-width: 100px;\" class=\"intask-cell\">".$arTask['DEADLINE']."</td>";
-			echo "<input type=\"hidden\" name=\"id\" value=\"".$arTask['ID']."\">";
-			echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\" type=\"submit\" style=\"cursor:pointer;\" name=\"getTask\" value=\"getTask\"/>Начать работу по задаче</button></td>";
-			echo "</tr>";
-			echo "</form>";
+			if ($arTask['PRIORITY'] == 2)
+			{
+				echo '<form action=/servicedesk/queue.php method="post">';
+				echo "<tr class=\"intask-row\">";
+				echo "<td class=\"intask-cell\"><font color=\"red\">".$arTask['ID']."</font></td>";
+				echo "<td class=\"intask-cell\"><font color=\"red\">".$arTask['CREATED_DATE']."</font></td>";
+				echo "<td class=\"intask-cell\"><font color=\"red\">".$arTask['CREATED_BY_LAST_NAME'], " ", $arTask['CREATED_BY_NAME'], " ",$arTask['CREATED_BY_SECOND_NAME']."</font></td>";
+				echo "<td class=\"intask-cell\"><a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/",$arTask['ID'],"/\">",$arTask['TITLE'],"</a></td>";
+				echo "<td style=\"min-width: 100px;\" class=\"intask-cell\"><font color=\"red\">".$arTask['DEADLINE']."</td>";
+				echo "<input type=\"hidden\" name=\"id\" value=\"".$arTask['ID']."\">";
+				echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\" type=\"submit\" style=\"cursor:pointer;\" name=\"getTask\" value=\"getTask\"/>Начать работу по задаче</button></td>";
+				echo "</tr>";
+				echo "</form>";
+			}
+			else
+			{
+				echo '<form action=/servicedesk/queue.php method="post">';
+				echo "<tr class=\"intask-row\">";
+				echo "<td class=\"intask-cell\">".$arTask['ID']."</td>";
+				echo "<td class=\"intask-cell\">".$arTask['CREATED_DATE']."</td>";
+				echo "<td class=\"intask-cell\">".$arTask['CREATED_BY_LAST_NAME'], " ", $arTask['CREATED_BY_NAME'], " ",$arTask['CREATED_BY_SECOND_NAME']."</td>";
+				echo "<td class=\"intask-cell\"><a href=\"https://in.autokontinent.ru/company/personal/user/", $USER->GetID(),"/tasks/task/view/",$arTask['ID'],"/\">",$arTask['TITLE'],"</a></td>";
+				echo "<td style=\"min-width: 100px;\" class=\"intask-cell\">".$arTask['DEADLINE']."</td>";
+				echo "<input type=\"hidden\" name=\"id\" value=\"".$arTask['ID']."\">";
+				echo "<td class=\"intask-cell\"><button class=\"ui-btn ui-btn-themes ui-btn-icon-setting ui-btn-icon-task tasks-quick-form-button\" type=\"submit\" style=\"cursor:pointer;\" name=\"getTask\" value=\"getTask\"/>Начать работу по задаче</button></td>";
+				echo "</tr>";
+				echo "</form>";
+			}
 		}
 	}
 	echo '</table>';
